430f81a041b409ac6ed502bacef7acb52ab094de
Anton Fediushin <fediushin.anton@yandex.com>
2018-07-21 08:55:24 +0300

    Add 99 bottles of beer example

---
+++ examples/99_bottles_of_beer/generate.d
@@ -0,0 +1,62 @@
+/+
+dub.json:
+{
+	"name": "generate"
+}
++/
+
+import std.format;
+import std.string;
+import std.file;
+import std.conv : to;
+
+enum TEMPLATE = q"EOS
+module %1$s;
+
+enum NUMBER_OF_BOTTLES = %2$d;
+
+enum BOTTLES = "%2$d bottle%3$s of beer on the wall\n%2$d bottle%3$s of beer\nTake one down, pass it around";
+
+unittest {
+	assert(NUMBER_OF_BOTTLES == %2$d);
+}
+
+EOS";
+
+enum APP = q"EOS
+module app;
+
+void main() {
+	import std.stdio;
+
+	string[] bottles;
+
+	%-(
+		
+	%)
+
+	foreach(bottle; bottles)
+		writeln(bottle);
+}
+EOS";
+
+void main() {
+	string directory = "source/";
+	string[] modules;
+
+	directory.mkdir;
+
+	foreach_reverse(i; 1..100) {
+		directory ~= "_%d_bottle%s/".format(i, i == 1 ? "" : "s");
+		directory.mkdir;
+
+		modules ~= directory["source/".length..$].tr("/", ".")[0..$-1] ~ ".beer";
+
+		write(directory ~ "/beer.d", format!TEMPLATE(modules[$-1], i, i == 1 ? "" : "s"));
+	}
+
+	"source/app.d".write("module app;\nimport std.stdio;\nversion(unittest) {} else void main() {\n\tstring[] bottles;\n");
+	foreach(m; modules)
+		"source/app.d".append(format!"\t{ import %s; bottles ~= BOTTLES; }\n"(m));
+	"source/app.d".append("\tforeach(b; bottles) writeln(b);\n}");
+}
