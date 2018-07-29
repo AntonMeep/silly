583875c2b30f47e94ce38872c2f9badb6492ca0f
Anton Fediushin <fediushin.anton@yandex.com>
2018-07-29 13:56:24 +0200

    Revert "Fix an issue when unittest aren't discoverable for some reason"
    This reverts commit 4b1d38f4136b05f28badf9b8f2042a15febf6c4e.
    Problem with it is that all of the things with nested modules stopped working

--- silly.d
+++ silly.d
@@ -56,8 +56,8 @@ void executeUnitTests() {
 	size_t workerCount;
 	scheduler.start({
 		auto started = MonoTime.currTime;
-		static foreach(m; __traits(getMember, dub_test_root, "allModules")) {
-			static foreach(test; __traits(getUnitTests, __traits(getMember, dub_test_root, __traits(identifier, m)))) {
+		static foreach(module_; __traits(getMember, dub_test_root, "allModules")) {
+			static foreach(test; __traits(getUnitTests, module_)) {
 				++workerCount;
 				spawn({
 					ownerTid.send(executeTest!test);
@@ -65,14 +65,7 @@ void executeUnitTests() {
 			}
 
 			version(SillyDebug)
-				pragma(msg, 
-					"silly | Module ",
-					fullyQualifiedName!(
-						__traits(getMember, dub_test_root, __traits(identifier, m))
-					).truncateName,
-					" contains ",
-					cast(int) __traits(getUnitTests,__traits(getMember, dub_test_root, __traits(identifier, m))).length,
-					" unittests");
+				pragma(msg, "silly | Module ", fullyQualifiedName!module_.truncateName, " contains ", cast(int) __traits(getUnitTests, module_).length, " unittests");
 		}
 
 		Array!TestResult results;
