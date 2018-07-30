0560880d04fddac41fd484351f3221a2bd0569f6
Anton Fediushin <fediushin.anton@yandex.com>
2018-07-30 14:29:42 +0200

    Move

---
+++ testsuite/feature.tests/simple/test.d
@@ -0,0 +1,24 @@
+module test;
+
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+unittest { assert(true); }
+
+
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
+unittest { assert(false); }
