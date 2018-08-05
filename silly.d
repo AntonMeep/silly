d81df3b3753e0bad0b46e21a15ad175ac7f7da01
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-05 19:39:19 +0200

    Display nice assert message when dub_test_root isn't found

--- silly.d
+++ silly.d
@@ -2,10 +2,11 @@ module silly;
 
 version(unittest):
 
-static if(!__traits(compiles, () {static import dub_test_root;}))
-	static assert(false, "silly | Couldn't find 'dub_test_root'. Make sure you are running tests with `dub test`");
-
-static import dub_test_root;
+static if(!__traits(compiles, () {static import dub_test_root;})) {
+	static assert(false, "Couldn't find 'dub_test_root'. Make sure you are running tests with `dub test`");
+} else {
+	static import dub_test_root;
+}
 
 import core.stdc.stdlib : exit;
 import core.time        : Duration, MonoTime, msecs;
