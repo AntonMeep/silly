bfcd15e5baaa4b762a8ace213d0cd2d5bd82457f
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-26 08:04:48 +0200

    Remove all of the unneccessary JS

--- pages/src/index.js
+++ pages/src/index.js
@@ -1,5 +1,1 @@
-window.$ = window.jQuery = require('jquery') // required for bootstrap
-window.Popper = require('popper.js') // required for tooltip, popup...
-require('bootstrap')
-
 import './index.scss' // include bootstrap css file with own modifications
