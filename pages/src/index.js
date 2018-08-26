93a5705135470d7437c309aa9ceb671bcc678dd0
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-26 10:10:08 +0200

    Update web pages so it will update silly's version automatically

--- pages/src/index.js
+++ pages/src/index.js
@@ -1,1 +1,11 @@
 import './index.scss' // include bootstrap css file with own modifications
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+	if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+		document.querySelectorAll(".version").forEach(function(e) {
+			e.textContent = "~>" + xmlHttp.responseText.replace(/['"]+/g, '');
+		});
+}
+xmlHttp.open("GET", "https://code.dlang.org/api/packages/silly/latest", true);
+xmlHttp.send(null);
