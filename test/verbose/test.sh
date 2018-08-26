14b02b4db7b3a085c30d8bc7c06bfaa7e5d5fded
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-26 08:43:01 +0200

    Add a test for --verbose option

---
+++ test/verbose/test.sh
@@ -0,0 +1,11 @@
+. $TESTSUITE/common.sh
+
+OUTPUT=$(dub test --root=$(dirname "${BASH_SOURCE[0]}") --skip-registry=all --nodeps -q -- --no-colours --verbose 2>&1 || true)
+
+echo "$OUTPUT" | grep -cE "✓ verbose 1 \(1000\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "✓ verbose 2 \(500\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "✓ verbose 3 \(250\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "../silly.d" > /dev/null
+echo "$OUTPUT" | grep -c "Summary: 3 passed, 1 failed" > /dev/null
+
+rm -r $(dirname "${BASH_SOURCE[0]}")/.dub $(dirname "${BASH_SOURCE[0]}")/verbose-test-unittest
