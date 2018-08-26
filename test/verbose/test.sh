d68a2377616fb3019722ef4b3775359bdbd4d797
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-26 08:44:19 +0200

    Fix test

--- test/verbose/test.sh
+++ test/verbose/test.sh
@@ -5,7 +5,7 @@ OUTPUT=$(dub test --root=$(dirname "${BASH_SOURCE[0]}") --skip-registry=all --no
 echo "$OUTPUT" | grep -cE "✓ verbose 1 \(1000\.[0-9]{3} ms\)" > /dev/null
 echo "$OUTPUT" | grep -cE "✓ verbose 2 \(500\.[0-9]{3} ms\)" > /dev/null
 echo "$OUTPUT" | grep -cE "✓ verbose 3 \(250\.[0-9]{3} ms\)" > /dev/null
-echo "$OUTPUT" | grep -cE "../silly.d" > /dev/null
+echo "$OUTPUT" | grep -cE "silly.d" > /dev/null
 echo "$OUTPUT" | grep -c "Summary: 3 passed, 1 failed" > /dev/null
 
 rm -r $(dirname "${BASH_SOURCE[0]}")/.dub $(dirname "${BASH_SOURCE[0]}")/verbose-test-unittest
