03d43fbb22ca04d89f93f4c62831bb3c0b95f47d
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-27 16:34:42 +0200

    Fix test which sometimes fails

--- test/verbose/test.sh
+++ test/verbose/test.sh
@@ -2,9 +2,9 @@
 
 OUTPUT=$(dub test --root=$(dirname "${BASH_SOURCE[0]}") --skip-registry=all --nodeps -q -- --no-colours --verbose 2>&1 || true)
 
-echo "$OUTPUT" | grep -cE "✓ verbose 1 \(1000\.[0-9]{3} ms\)" > /dev/null
-echo "$OUTPUT" | grep -cE "✓ verbose 2 \(500\.[0-9]{3} ms\)" > /dev/null
-echo "$OUTPUT" | grep -cE "✓ verbose 3 \(250\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "✓ verbose 1 \(100[0-9]\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "✓ verbose 2 \(50[0-9]\.[0-9]{3} ms\)" > /dev/null
+echo "$OUTPUT" | grep -cE "✓ verbose 3 \(25[0-9]\.[0-9]{3} ms\)" > /dev/null
 echo "$OUTPUT" | grep -cE "silly.d" > /dev/null
 echo "$OUTPUT" | grep -c "Summary: 3 passed, 1 failed" > /dev/null
 
