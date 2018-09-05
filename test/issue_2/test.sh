06f26fc78831b69c8b093503551a36a3fb756a3a
Anton Fediushin <fediushin.anton@yandex.com>
2018-09-05 16:42:03 +0200

    Fix tests

--- test/issue_2/test.sh
+++ test/issue_2/test.sh
@@ -4,7 +4,7 @@ OUTPUT=$(dub test -b unittest-cov --root=$(dirname "${BASH_SOURCE[0]}") --skip-r
 
 echo "$OUTPUT" | grep -c "✗ issue_2 Throwing an exception with multi-line message" > /dev/null
 echo "$OUTPUT" | grep -c "Summary: 0 passed, 1 failed" > /dev/null
-echo "$OUTPUT" | grep -cE "^\s+Hello," > /dev/null
+echo "$OUTPUT" | grep -cE "\s+Hello," > /dev/null
 echo "$OUTPUT" | grep -cE "^\s+World!" > /dev/null
 
 rm -r $(dirname "${BASH_SOURCE[0]}")/.dub $(dirname "${BASH_SOURCE[0]}")/issue_2-test-unittest
